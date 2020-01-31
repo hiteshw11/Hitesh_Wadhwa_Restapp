d82c36025338ad2c36904675e3eb494fcf673fcc
hiteshw11 <50346971+hiteshw11@users.noreply.github.com>
2020-01-31 12:14:12 +0000

    Update Restdata.java

--- src/packer/Restdata.java
+++ src/packer/Restdata.java
@@ -43,7 +43,7 @@ public class Restdata extends HttpServlet {
 try {
 			
 			Class.forName("com.mysql.jdbc.Driver"); 
-			java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://us-cdbr-iron-east-02.cleardb.net/heroku_a37659de952362a	","b65974ec261c44","b755a171"); 
+			java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://us-cdbr-iron-east-02.cleardb.net/DB_NAME","USERNAME","PASSWORD"); 
 			Statement st= con.createStatement(); 
 			ResultSet rs=st.executeQuery("select * from onetwo");
 			java.sql.ResultSetMetaData rsmd = rs.getMetaData();
